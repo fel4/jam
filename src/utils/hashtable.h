2da7b7fd6150026bfde226d0e342847a76706a32
Cary Haynie <cary.haynie@gmail.com>
2017-03-15 12:14:37 -0400

    add skeleton impls for a rope structure and a hashtable structure.

---
+++ src/utils/hashtable.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef JAM_UTILS_HASHTABLE_H
+#define JAM_UTILS_HASHTABLE_H
+
+#include "jam/jam.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+typedef struct jam_hashtable_s jam_hashtable_t;
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif /* JAM_UTILS_HASHTABLE_H */
---
+++ src/utils/rope.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef JAM_UTILS_ROPE_H
+#define JAM_UTILS_ROPE_H
+
+#include "jam/jam.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+typedef struct jam_rope_s jam_rope_t;
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif /* JAM_UTILS_ROPE_H */
