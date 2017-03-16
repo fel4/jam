622ae782652af1d388296180e3672db9ce2e098f
Cary Haynie <cary.haynie@gmail.com>
2017-03-16 19:13:17 -0400

    flush out rest of fnv1a hash module.

--- src/utils/fnv1a.c
+++ src/utils/fnv1a.c
@@ -1,6 +1,5 @@
 #include "fnv1a.h"
 #include "../alloc.h"
-#include <stdint.h>
 
 static const uint64_t fnv_offset_basis = 0xcbf29ce484222325;
 static const uint64_t fnv_prime = 0x100000001b3;
@@ -23,6 +22,36 @@ static unit64_t fnv_hash_pass(jam_fnv1a_hash_t* hash, const char* buffer, size_t
 
 /* begin public api */
 
+jam_result_t jam_fnv1a_hash_new(jam_allocator_t* allocator, jam_fnv1a_hash_t** hash)
+{
+    *hash = _MALLOC(allocator, sizeof(jam_fnv1a_hash_t));
+    if ((*hash) == NULL) return JAM_RESULT_NOMEM;
+    (*hash)->hash = fnv_offset_basis;
+    return JAM_RESULT_OK;
+}
 
+jam_result_t jam_fnv1a_hash_destroy(jam_fnv1a_hash_t* hash, jam_allocator_t* allocator)
+{
+    _FREE(allocator, hash);
+    return JAM_RESULT_OK;
+}
+
+jam_result_t jam_fnv1a_hash_add(jam_fnv1a_hash_t* hash, const char* buffer, size_t buf_siz, uint64_t* out_hash)
+{
+    if (hash == NULL) return JAM_RESULT_BADARG;
+    if (buffer == NULL) return JAM_RESULT_BADARG;
+    if (buf_siz == 0) return JAM_RESULT_BADARG;
+    uint64_t tmp = fnv_hash_pass(hash, buffer, buf_siz);
+    if (out_hash != NULL) *out_hash = tmp;
+    return JAM_RESULT_OK;
+}
+
+jam_result_t jam_fnv1a_hash_get(jam_fnv1a_hash_t* hash, uint64_t* out_hash)
+{
+    if (hash == NULL) return JAM_RESULT_BADARG;
+    if (out_hash == NULL) return JAM_RESULT_BADARG;
+    *out_hash = hash->hash;
+    return JAM_RESULT_OK;
+}
 
 /* end public api */
--- src/utils/fnv1a.h
+++ src/utils/fnv1a.h
@@ -4,12 +4,16 @@
 #define JAM_UTILS_FNV1A_H
 
 #include "jam/jam.h"
+#include <stdint.h>
 
 BEGIN_C_EXTERN
 
 typedef struct jam_fnv1a_hash_s jam_fnv1a_hash_t;
 
 jam_result_t jam_fnv1a_hash_new(jam_allocator_t*, jam_fnv1a_hash_t**);
+jam_result_t jam_fnv1a_hash_destroy(jam_fnv1a_hash_t*, jam_allocator_t*);
+jam_result_t jam_fnv1a_hash_add(jam_fnv1a_hash_t*, const char*, size_t, uint64_t*);
+jam_result_t jam_fnv1a_hash_get(jam_fnv1a_hash_t*, uint64_t*);
 
 END_C_EXTERN
 #endif /* JAM_UTILS_FNV1A_H */
